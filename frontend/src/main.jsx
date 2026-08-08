95043ee9220f7c206d12e2bd032a5175d9585997
moises12366-stack <moises12366@gmail.com>
2026-08-07 23:22:58 -0500

    Convertir Vivekfe en aplicacion PWA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import App from "./App.jsx";
+
+
+registerSW();
+
+
+createRoot(document.getElementById("root")).render(
+
   <StrictMode>
+
     <App />
-  </StrictMode>,
-)
+
+  </StrictMode>
+
+);
